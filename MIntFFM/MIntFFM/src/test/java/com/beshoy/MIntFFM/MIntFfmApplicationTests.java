1e4a5b1636fb3bd2816db5d60ac037b22b897920
Beshoy Farag <beshoynady401@gmail.com>
2026-05-08 16:12:23 +0200

    add the dependencies unfinished

---
+++ MIntFFM/MIntFFM/src/test/java/com/beshoy/MIntFFM/MIntFfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beshoy.MIntFFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MIntFfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
